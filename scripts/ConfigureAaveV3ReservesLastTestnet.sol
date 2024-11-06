9f0fadafc4049126a79092568f1f2073e2fd49f0
alphak3y <alphak3y@protonmail.com>
2024-11-06 12:07:39 -0700

    add package json script

--- scripts/ConfigureAaveV3ReservesLastTestnet.sol
+++ scripts/ConfigureAaveV3ReservesLastTestnet.sol
@@ -5,7 +5,6 @@ import {Script} from 'forge-std/Script.sol';
 import 'forge-std/StdJson.sol';
 import 'forge-std/console.sol';
 
-import '../../src/deployments/interfaces/IMarketReportTypes.sol';
 import {DeployUtils} from '../../src/deployments/contracts/utilities/DeployUtils.sol';
 import {LastTestnetReservesConfig} from 'src/deployments/configs/LastTestnetReservesConfig.sol';
 
